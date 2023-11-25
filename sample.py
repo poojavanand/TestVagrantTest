1c0787d71e70cf98d64504cea4ddefabb547ba1d
pooja v anand <poojavanand555@gmail.com>
2023-11-25 20:14:17 +0530

    commit

--- sample.py
+++ sample.py
@@ -6,22 +6,22 @@ def discount_calc(unit_price,quantity):
         return f_amount
     else:
         return amount
-def gst_calculator(amount,gst):
+def gst_cal(amount,gst):
     gst = amount * float(gst/100)
 
     return gst
 
 product_list = [["Leather wallet",1100,18,1],["Umbrella",900,12,4],["cigarette",200,28,3],["Honey",100,0,2]]
 length = len(product_list)
-final_price = []
-max_gst = []
-final_amount = []
+fprice = []
+mgst = []
+final_amt = []
 for i in range(length):
     #for j in range(i+1):
     result1 = discount_calc(product_list[i][1],product_list[i][3])
-    result2 = gst_calculator(result1,product_list[i][2])
-    final_price1 = result1 + result2
-    final_amount.append(final_price1)
-    max_gst.append(result2)
-print("final amount to be payed",sum(final_amount))
-print("maximum gst is ",max(max_gst),"for the product",product_list[1][0])
+    result2 = gst_cal(result1,product_list[i][2])
+    fprice1 = result1 + result2
+    final_amt.append(fprice1)
+    mgst.append(result2)
+print("final amount to be payed",sum(final_amt))
+print("maximum gst is ",max(mgst),"for the product",product_list[1][0])
